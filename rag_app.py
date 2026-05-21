47ad761c2850e63306901ba0eea1a099b36c81e9
Adam Qureshi <adam.y.qureshi@gmail.com>
2026-05-20 21:20:36 -0400

    Added new functionality to actually call the API for a repsonse

--- rag_app.py
+++ rag_app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 #web framework that handles the HTTP requests and responses
 from fastapi import FastAPI
+from google import genai
 
 load_dotenv()
 
@@ -12,14 +13,22 @@ api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("GEMINI_API_KEY is not set in the .env file")
 
-
-#This creates an instance of the web server.
-#Think of this as your "Main" application object. 
-#It’s the engine that will listen for connections.
+client = genai.Client()
 app = FastAPI()
 
-#Here, it tells FastAPI:Here, it tells FastAPI: "Whenever a user visits the URL path /health using a GET request, run this health_check function and send the result back to them." "Whenever a user visits the URL path /health using a GET request, 
-#run this health_check function and send the result back to them."
+
 @app.get("/health")
 def health_check():
     return {"status": "running", "api_key_loaded": bool(api_key)}
+
+@app.get("/test-gemini")
+def test_gemini():
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents="Explain what an LLM is and how it works!"
+        )
+        return {"response": response.text}
+
+    except Exception as e:
+        raise ValueError(f"Gemini API Call Failed: {str(e)}")
